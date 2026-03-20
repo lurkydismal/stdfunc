3fb991dd0c7ea0b7a1d95a486cd533b84f07a36a
LurkyDismal <lurkydismal@duck.com>
2026-03-20 15:21:57 +0000

    Implement STDCOLOR_NO_BOLD macro guard

--- include/stdcolor.hpp
+++ include/stdcolor.hpp
@@ -4,15 +4,34 @@
 
 namespace stdfunc::color {
 
-constexpr std::string_view g_white = "\x1b[1;37m";
-constexpr std::string_view g_cyanLight = "\x1b[1;36m";
-constexpr std::string_view g_blueLight = "\x1b[1;34m";
-constexpr std::string_view g_green = "\x1b[1;32m";
-constexpr std::string_view g_purpleLight = "\x1b[1;35m";
-constexpr std::string_view g_red = "\x1b[1;31m";
-constexpr std::string_view g_yellow = "\x1b[1;33m";
-constexpr std::string_view g_resetForeground = "\x1b[39m";
-constexpr std::string_view g_resetBackground = "\x1b[49m";
-constexpr std::string_view g_reset = "\x1b[0m";
+#define COLOR_START "\x1b["
+
+#if defined( STDCOLOR_NO_BOLD )
+
+#define TEXT_FORMATTING "0;"
+
+#else
+
+#define TEXT_FORMATTING "1;"
+
+#endif
+
+#define COLOR_END "m"
+
+constexpr std::string_view g_white = COLOR_START TEXT_FORMATTING "37" COLOR_END;
+constexpr std::string_view g_cyanLight =
+    COLOR_START TEXT_FORMATTING "36" COLOR_END;
+constexpr std::string_view g_blueLight =
+    COLOR_START TEXT_FORMATTING "34" COLOR_END;
+constexpr std::string_view g_green = COLOR_START TEXT_FORMATTING "32" COLOR_END;
+constexpr std::string_view g_purpleLight =
+    COLOR_START TEXT_FORMATTING "35" COLOR_END;
+constexpr std::string_view g_red = COLOR_START TEXT_FORMATTING "31" COLOR_END;
+constexpr std::string_view g_yellow =
+    COLOR_START TEXT_FORMATTING "33" COLOR_END;
+
+constexpr std::string_view g_resetForeground = COLOR_START "39" COLOR_END;
+constexpr std::string_view g_resetBackground = COLOR_START "49" COLOR_END;
+constexpr std::string_view g_reset = COLOR_START "0" COLOR_END;
 
 } // namespace stdfunc::color
